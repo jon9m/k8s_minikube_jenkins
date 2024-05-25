53a2c4728aec5ebee91e345559788d4c33487ee6
jon9m <manojam79@gmail.com>
2024-05-25 22:11:25 +1000

    tests

--- src/test/java/com/mmk/restapi/RestapiApplicationTests.java
+++ src/test/java/com/mmk/restapi/RestapiApplicationTests.java
@@ -1,13 +1,31 @@
 package com.mmk.restapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RestapiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void testRestApi() {
+        String body = restTemplate.getForObject("http://localhost:" + port + "/iss/people", String.class);
+        assertThat(body, containsString("Oleg Kononenko"));
+    }
 
 }
